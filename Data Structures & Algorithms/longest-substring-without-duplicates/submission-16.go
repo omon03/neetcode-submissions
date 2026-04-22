bd1d10dac36582d1fad61c8b93b99cdaec0044b6
omon03 <omon03@gmail.com>
2026-04-22 18:22:42 +0500

    Add: longest-substring-without-duplicates - submission-16

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-16.go
@@ -0,0 +1,30 @@
+func lengthOfLongestSubstring(s string) int {
+	rez := 0
+	buf := 0
+	r := []rune(s)
+	m := make(map[rune]int, len(r))
+
+	for i := 0; i < len(r); i++ {
+		if len(r) - i + buf < rez {
+			break
+		}
+		for j := i; j < len(r); j++ {
+			v := r[j]
+			if _, ok := m[v]; !ok {
+				m[v] = j
+				buf++
+				if buf > rez {
+					rez = buf
+				}
+			} else {
+				i = m[v]
+				m = make(map[rune]int, len(r) - i)
+				buf = 0
+				break
+			}
+		}
+	}
+	return rez
+}
+// thequickbrownf oxjumpsoverthelazydogthequickbrownfoxjumpsovert
+// thequickbro wnfoxjumps overthelazydogthequickbrownfoxjumpsovert
